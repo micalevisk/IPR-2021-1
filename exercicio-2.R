9c1a0ed3eb7f1a5dad81903509169990ed532948
Micael Levi (lab) <mllc@icomp.ufam.edu.br>
2021-06-02 15:56:50 -0400

    add resposta para o homework 2 (01/06)

---
+++ exercicio-2.R
@@ -0,0 +1,11 @@
+##
+## aluno: Micael Levi L. C. - 21554923
+## versão: 02-06-2021
+##
+
+################################################################################
+## Crie um algoritmo para acumular 100 valores de números aleatórios usando a função runif(1)
+################################################################################
+soma=0
+for (e in 1:100) soma = soma + runif(1)
+show(soma)
